12a33a3417a28944a504003e7eb1822402b0a06c
William Yallop <hello@williamyallop.com>
2023-05-31 18:46:45 +0100

    Removed user lookup in authenticate middleware as wont be needed with soft user delete

--- packages/core/src/middleware/authenticate.ts
+++ packages/core/src/middleware/authenticate.ts
@@ -1,7 +1,6 @@
 import { Request, Response, NextFunction } from "express";
-import client from "@db/db";
 // Services
-import { verifyJWT, clearJWT } from "@services/auth/jwt";
+import { verifyJWT } from "@services/auth/jwt";
 // Utils
 import { LucidError } from "@utils/error-handler";
 
@@ -13,21 +12,6 @@ const authenticate = async (
   try {
     const authenticateJWT = verifyJWT(req);
     if (!authenticateJWT.sucess || !authenticateJWT.data) {
-      throw new LucidError({
-        type: "authorisation",
-        message: "You are not authorised to perform this action",
-      });
-    }
-
-    const user = await client.query({
-      text: `SELECT id FROM lucid_users WHERE id = $1`,
-      values: [authenticateJWT.data.id],
-    });
-
-    if (!user.rows[0]) {
-      // clear cookie
-      clearJWT(res);
-      // throw error
       throw new LucidError({
         type: "authorisation",
         message: "You are not authorised to perform this action",
