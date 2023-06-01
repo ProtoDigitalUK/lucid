826f71370ff10872ee177a076ce282af6cc81f4a
William Yallop <hello@williamyallop.com>
2023-06-01 07:17:30 +0100

    Updated migration file to support collections and removed post types

--- packages/core/src/db/migrations/00000004-collections-pages-categories.sql
+++ packages/core/src/db/migrations/00000004-collections-pages-categories.sql
@@ -1,19 +1,24 @@
--- POST TYPES TABLE
-CREATE TABLE IF NOT EXISTS lucid_post_types (
-  id SERIAL PRIMARY KEY,
+-- COLLECTIONS TABLE
+CREATE TYPE collection_type AS ENUM ('single', 'multiple');
 
-  key TEXT UNIQUE NOT NULL,
-  name TEXT NOT NULL,
-  singular_name TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS lucid_collections (
+  id SERIAL PRIMARY KEY,
+  key: TEXT UNIQUE NOT NULL,
+
+  title TEXT NOT NULL,
+  singular TEXT NOT NULL,
+  description TEXT,
+  type collection_type DEFAULT 'multiple',
+
+  created_at TIMESTAMP DEFAULT NOW(),
+  updated_at TIMESTAMP DEFAULT NOW()
 );
 
-INSERT INTO lucid_post_types (key, name, singular_name) VALUES ('page', 'Pages', 'Page');
-
--- PAGES TABLE
+-- PAGES TABLE - used for multiple collections
 CREATE TABLE IF NOT EXISTS lucid_pages (
   id SERIAL PRIMARY KEY,
-  post_type_id INTEGER NOT NULL REFERENCES lucid_post_types(id) ON DELETE CASCADE,
   parent_id INTEGER REFERENCES lucid_pages(id) ON DELETE SET NULL,
+  collection_key TEXT REFERENCES lucid_collections(key),
 
   title TEXT NOT NULL,
   slug TEXT NOT NULL,
@@ -30,10 +35,23 @@ CREATE TABLE IF NOT EXISTS lucid_pages (
   updated_at TIMESTAMP DEFAULT NOW()
 );
 
+-- GROUP TABLE - used for single collections to show custom settings, footer, header pages etc
+CREATE TABLE IF NOT EXISTS lucid_groups (
+  id SERIAL PRIMARY KEY,
+  collection_key TEXT REFERENCES lucid_collections(key),
+
+  name TEXT NOT NULL,
+  slug TEXT UNIQUE NOT NULL,
+  description TEXT,
+
+  created_at TIMESTAMP DEFAULT NOW(),
+  updated_at TIMESTAMP DEFAULT NOW()
+);
+
 -- CATEGORIES TABLE
 CREATE TABLE IF NOT EXISTS lucid_categories (
   id SERIAL PRIMARY KEY,
-  post_type_id INTEGER NOT NULL REFERENCES lucid_post_types(id) ON DELETE CASCADE,
+  collection_key TEXT REFERENCES lucid_collections(key) ON DELETE CASCADE,
 
   title TEXT NOT NULL,
   slug TEXT NOT NULL,  -- unique per post type
