99ccc52c18aebceb7a19169bee3e2c76c59e1a0e
William Yallop <hello@williamyallop.com>
2023-06-07 07:35:49 +0100

    Updated how full_slugs are worked out, now uses a trigger to set the full_slug based off its own slug and its parents. This works recursivly

--- packages/core/src/db/migrations/00000004-pages-categories.sql
+++ packages/core/src/db/migrations/00000004-pages-categories.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS lucid_pages (
   collection_key TEXT NOT NULL,
 
   title TEXT NOT NULL,
-  slug TEXT NOT NULL,
-  full_slug TEXT NOT NULL, -- kept unqiue in code
+  slug TEXT,
+  full_slug TEXT, 
   homepage BOOLEAN DEFAULT FALSE,
   excerpt TEXT,
   
@@ -18,6 +18,46 @@ CREATE TABLE IF NOT EXISTS lucid_pages (
   created_at TIMESTAMP DEFAULT NOW(),
   updated_at TIMESTAMP DEFAULT NOW()
 );
+
+-- create full_slug
+CREATE OR REPLACE FUNCTION update_full_slug() RETURNS trigger AS $$
+DECLARE
+    parent_full_slug TEXT;
+BEGIN
+    -- Get parent's full_slug
+    SELECT full_slug INTO parent_full_slug FROM lucid_pages WHERE id = NEW.parent_id;
+    
+    -- Compute new full_slug for the updated/inserted row
+    IF parent_full_slug IS NULL THEN
+        NEW.full_slug := NEW.slug;
+    ELSE
+        NEW.full_slug := parent_full_slug || '/' || NEW.slug;
+    END IF;
+    
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS update_full_slug_trigger ON lucid_pages;
+CREATE TRIGGER update_full_slug_trigger BEFORE INSERT OR UPDATE ON lucid_pages
+FOR EACH ROW EXECUTE PROCEDURE update_full_slug();
+
+CREATE OR REPLACE FUNCTION update_child_full_slug() RETURNS trigger AS $$
+BEGIN
+    -- If it's an UPDATE operation and slug is changed
+    IF OLD.slug != NEW.slug THEN
+        -- Update children's full_slug
+        UPDATE lucid_pages SET full_slug = NEW.full_slug || substring(full_slug, length(OLD.full_slug) + 1)
+        WHERE full_slug LIKE OLD.full_slug || '/%';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS update_child_full_slug_trigger ON lucid_pages;
+CREATE TRIGGER update_child_full_slug_trigger AFTER UPDATE ON lucid_pages
+FOR EACH ROW WHEN (OLD.slug IS DISTINCT FROM NEW.slug) EXECUTE PROCEDURE update_child_full_slug();
 
 -- GROUP TABLE - collection group type, is used as a intermediary table for the collections bricks
 CREATE TABLE IF NOT EXISTS lucid_groups (
