30e9396ce7954b43e81c1db18f5ce516c43dba7c
William Yallop <hello@williamyallop.com>
2023-09-04 19:49:00 +0100

    Added new settings page ready for content

--- apps/cms/src/routes/Settings/List.tsx
+++ apps/cms/src/routes/Settings/List.tsx
@@ -1,12 +1,22 @@
+import T from "@/translations";
 import { Component } from "solid-js";
+// Componetns
+import Layout from "@/components/Groups/Layout";
 
 const SettingsListRoute: Component = () => {
   // ----------------------------------
   // Render
   return (
-    <div>
-      <h1>Settings List</h1>
-    </div>
+    <Layout.PageLayout
+      title={T("settings_route_title")}
+      description={T("settings_route_description")}
+      options={{
+        noPadding: true,
+      }}
+      headingChildren={<>gello</>}
+    >
+      body
+    </Layout.PageLayout>
   );
 };
 
