044ff7d44ebf37c142eadc3b5a1d836a54cecddc
William Yallop <hello@williamyallop.com>
2024-02-15 22:27:18 +0000

    Package now exports sendEmail, serviceWrapper and headlessDB and created an endpoint in headless-example to use and test sendEmail service that headless package exports

--- apps/headless-example/src/index.ts
+++ apps/headless-example/src/index.ts
@@ -1,6 +1,6 @@
 import Fastify from "fastify";
 import { log } from "console-log-colors";
-import headless from "@protodigital/headless";
+import headless, { sendEmail } from "@protodigital/headless";
 
 const fastify = Fastify({
 	logger: true,
@@ -9,6 +9,21 @@ const fastify = Fastify({
 fastify.register(headless);
 
 const port = Number(process.env.PORT) || 8393;
+
+fastify.post("/send-email", async (request, reply) => {
+	const res = await sendEmail({
+		to: "hello@williamyallop.com",
+		subject: "Hello",
+		template: "contact-form",
+		data: {
+			name: "William",
+			email: "hello@williamyallop.com",
+			message: "Hello!",
+		},
+	});
+
+	reply.send(res);
+});
 
 await fastify.listen({
 	port: port,
--- packages/headless/src/index.ts
+++ packages/headless/src/index.ts
@@ -3,8 +3,19 @@ import headless from "./headless.js";
 import { headlessConfig } from "./services/config.js";
 import BrickBuilder from "./builders/brick-builder/index.js";
 import CollectionBuilder from "./builders/collection-builder/index.js";
+import sendEmail from "./services/email/send-external.js";
+import serviceWrapper from "./utils/app/service-wrapper.js";
+import { headlessDB } from "./db/db.js";
 
-export { headless, headlessConfig, BrickBuilder, CollectionBuilder };
+export {
+	headless,
+	headlessConfig,
+	BrickBuilder,
+	CollectionBuilder,
+	sendEmail,
+	serviceWrapper,
+	headlessDB,
+};
 export type { EmailStrategyT } from "./schemas/config.js";
 
 export default headless;
