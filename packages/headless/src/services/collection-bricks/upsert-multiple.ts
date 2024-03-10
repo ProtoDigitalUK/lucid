93c811c5dd66123c9f5d21e0a6ca93423462ef86
William Yallop <hello@williamyallop.com>
2024-03-10 20:43:34 +0000

    WIP solution for upserting groups and going back to update parent_ids after groups have been added in the first place

--- packages/headless/src/services/collection-bricks/upsert-multiple.ts
+++ packages/headless/src/services/collection-bricks/upsert-multiple.ts
@@ -85,15 +85,94 @@ const upsertMultiple = async (
 	// update data.bricks with the new ids where key and order match
 	data.bricks = assignBrickIds(data.bricks, bricksRes);
 
-	const newGroups = await serviceWrapper(
-		collectionBricksServices.createMultipleGroups,
-		false,
-	)(serviceConfig, { bricks: data.bricks });
+	const groupsRes = await serviceConfig.db
+		.insertInto("headless_groups")
+		.values(
+			data.bricks.flatMap((brick) => {
+				if (!brick.groups) return [];
 
-	// update data.bricks with the new group ids
-	const groups = assignGroupsParentIds(data.bricks, newGroups);
+				return brick.groups.map((group) => {
+					return {
+						group_id:
+							typeof group.group_id === "string"
+								? undefined
+								: group.group_id,
+						parent_group_id:
+							typeof group.parent_group_id === "string"
+								? undefined
+								: group.parent_group_id,
+						collection_brick_id: brick.id as number,
+						group_order: group.group_order,
+						repeater_key: group.repeater_key,
+						language_id: group.language_id,
+						ref:
+							typeof group.group_id === "string"
+								? group.group_id
+								: undefined,
+					};
+				});
+			}),
+		)
+		.onConflict((oc) =>
+			oc.column("group_id").doUpdateSet((eb) => ({
+				group_order: eb.ref("excluded.group_order"),
+				parent_group_id: eb.ref("excluded.parent_group_id"),
+				ref: eb.ref("excluded.ref"),
+			})),
+		)
+		.returning(["group_id", "ref"])
+		.execute();
 
-	// update existing groups with order and parent_group_id
+	// TODO: if data.bricks.groups has a parent_group_id, update the parent_group_id with the new group id. Also update group_id if it's a ref
+	const groups = data.bricks.flatMap((brick) => {
+		if (!brick.groups) return [];
+		return brick.groups.map((group) => {
+			if (
+				typeof group.group_id === "string" &&
+				group.group_id.startsWith("ref-")
+			) {
+				const foundGroup = groupsRes.find(
+					(res) => res.ref === group.group_id,
+				);
+				if (!foundGroup) throw new Error("Group not found");
+				group.group_id = foundGroup.group_id;
+			}
+			if (
+				typeof group.parent_group_id === "string" &&
+				group.parent_group_id.startsWith("ref-")
+			) {
+				const foundGroup = groupsRes.find(
+					(res) => res.ref === group.parent_group_id,
+				);
+				if (!foundGroup) throw new Error("Group not found");
+				group.parent_group_id = foundGroup.group_id;
+			}
+
+			return group;
+		});
+	});
+	// TODO: update groups by the id with their new parent_group_id
+	const updatedGroups = await serviceConfig.db
+		.insertInto("headless_groups")
+		.values(
+			groups.map((group) => {
+				return {
+					group_id: group.group_id as number,
+					parent_group_id: group.parent_group_id as number | null,
+					group_order: group.group_order as number,
+					repeater_key: group.repeater_key,
+					language_id: group.language_id,
+				};
+			}),
+		)
+		.onConflict((oc) =>
+			oc.column("group_id").doUpdateSet((eb) => ({
+				parent_group_id: eb.ref("excluded.parent_group_id"),
+			})),
+		)
+		.execute();
+
+	// TODO: update data.bricks fields with the new group ids
 };
 
 const assignBrickIds = (
@@ -122,36 +201,36 @@ const assignBrickIds = (
 	});
 };
 
-const assignGroupsParentIds = (
-	bricks: Array<BrickObjectT>,
-	groups: Array<{
-		group_id: number;
-		ref: string;
-	}>,
-) => {
-	const newGroupMap: { [key: string]: number } = {};
-	for (const group of groups) {
-		newGroupMap[group.ref] = group.group_id;
-	}
-	const allGroups = [];
+// const assignGroupsParentIds = (
+// 	bricks: Array<BrickObjectT>,
+// 	groups: Array<{
+// 		group_id: number;
+// 		ref: string;
+// 	}>,
+// ) => {
+// 	const newGroupMap: { [key: string]: number } = {};
+// 	for (const group of groups) {
+// 		newGroupMap[group.ref] = group.group_id;
+// 	}
+// 	const allGroups = [];
 
-	for (const brick of bricks) {
-		if (!brick.groups) continue;
+// 	for (const brick of bricks) {
+// 		if (!brick.groups) continue;
 
-		for (const group of brick.groups) {
-			if (!group.parent_group_id) {
-				allGroups.push(group);
-				continue;
-			}
+// 		for (const group of brick.groups) {
+// 			if (!group.parent_group_id) {
+// 				allGroups.push(group);
+// 				continue;
+// 			}
 
-			if (newGroupMap[group.parent_group_id]) {
-				group.parent_group_id = newGroupMap[group.parent_group_id];
-				allGroups.push(group);
-			}
-		}
-	}
+// 			if (newGroupMap[group.parent_group_id]) {
+// 				group.parent_group_id = newGroupMap[group.parent_group_id];
+// 				allGroups.push(group);
+// 			}
+// 		}
+// 	}
 
-	return allGroups;
-};
+// 	return allGroups;
+// };
 
 export default upsertMultiple;
