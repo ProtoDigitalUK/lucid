dc21e352bb50c6c74512e37ee5dbe6a3a4e692d6
William Yallop <hello@williamyallop.com>
2024-04-04 22:02:37 +0100

    Collection document fields queries moved into own repo class

--- packages/headless/src/repositories/collection-document-fields.ts
+++ packages/headless/src/repositories/collection-document-fields.ts
@@ -1,22 +1,75 @@
-import type {
-	HeadlessCollectionDocumentFields,
-	Select,
-} from "../libs/db/types.js";
-import {
-	deleteQB,
-	selectQB,
-	type QueryBuilderWhereT,
-} from "../libs/db/query-builder.js";
+import type { BooleanInt } from "../libs/db/types.js";
+import { deleteQB, type QueryBuilderWhereT } from "../libs/db/query-builder.js";
+import type { FieldTypesT } from "../libs/builders/field-builder/index.js";
 
 export default class CollectionDocumentFieldsRepo {
 	constructor(private db: DB) {}
 
 	// ----------------------------------------
-	// selects
-	// ----------------------------------------
-	// create
-	// ----------------------------------------
-	// update
+	// upsert
+	upsertMultiple = async (props: {
+		items: Array<{
+			fieldsId?: number | undefined;
+			collectionDocumentId: number;
+			collectionBrickId: number;
+			key: string;
+			type: FieldTypesT;
+			groupId?: number | null;
+			textValue: string | null;
+			intValue: number | null;
+			boolValue: BooleanInt | null;
+			jsonValue: string | null;
+			pageLinkId: number | null;
+			mediaId: number | null;
+			languageId: number;
+		}>;
+	}) => {
+		return this.db
+			.insertInto("headless_collection_document_fields")
+			.values(
+				props.items.map((f) => {
+					return {
+						fields_id: f.fieldsId,
+						collection_document_id: f.collectionDocumentId,
+						collection_brick_id: f.collectionBrickId,
+						key: f.key,
+						type: f.type,
+						group_id: f.groupId,
+						text_value: f.textValue,
+						int_value: f.intValue,
+						bool_value: f.boolValue,
+						json_value: f.jsonValue,
+						page_link_id: f.pageLinkId,
+						media_id: f.mediaId,
+						language_id: f.languageId,
+					};
+				}),
+			)
+			.onConflict((oc) =>
+				oc.column("fields_id").doUpdateSet((eb) => ({
+					text_value: eb.ref("excluded.text_value"),
+					int_value: eb.ref("excluded.int_value"),
+					bool_value: eb.ref("excluded.bool_value"),
+					json_value: eb.ref("excluded.json_value"),
+					page_link_id: eb.ref("excluded.page_link_id"),
+					media_id: eb.ref("excluded.media_id"),
+					group_id: eb.ref("excluded.group_id"),
+				})),
+			)
+			.returning(["fields_id"])
+			.execute();
+	};
 	// ----------------------------------------
 	// delete
+	deleteMultiple = async (props: {
+		where: QueryBuilderWhereT<"headless_collection_document_fields">;
+	}) => {
+		let query = this.db
+			.deleteFrom("headless_collection_document_fields")
+			.returning(["fields_id"]);
+
+		query = deleteQB(query, props.where);
+
+		return query.execute();
+	};
 }
--- packages/headless/src/services/collection-document-bricks/upsert-multiple-fields.ts
+++ packages/headless/src/services/collection-document-bricks/upsert-multiple-fields.ts
@@ -1,6 +1,7 @@
 import type { BrickSchemaT } from "../../schemas/collection-bricks.js";
 import formatUpsertFields from "../../format/format-upsert-fields.js";
 import type { GroupsResT } from "./upsert-multiple-groups.js";
+import RepositoryFactory from "../../libs/factories/repository-factory.js";
 
 export interface ServiceData {
 	document_id: number;
@@ -24,56 +25,47 @@ const upsertMultipleFields = async (
 		return;
 	}
 
+	const CollectionDocumentFieldsRepo = RepositoryFactory.getRepository(
+		"collection-document-fields",
+		serviceConfig.db,
+	);
+
 	// upsert fields
-	const fieldsRes = await serviceConfig.db
-		.insertInto("headless_collection_document_fields")
-		.values(
-			fields.map((field) => {
-				return {
-					fields_id: field.fields_id ?? undefined,
-					collection_document_id: data.document_id,
-					collection_brick_id: field.collection_brick_id,
-					key: field.key,
-					type: field.type,
-					group_id: field.group_id,
-					text_value: field.text_value,
-					int_value: field.int_value,
-					bool_value: field.bool_value,
-					json_value: field.json_value,
-					page_link_id: field.page_link_id,
-					media_id: field.media_id,
-					language_id: field.language_id,
-				};
-			}),
-		)
-		.onConflict((oc) =>
-			oc.column("fields_id").doUpdateSet((eb) => ({
-				text_value: eb.ref("excluded.text_value"),
-				int_value: eb.ref("excluded.int_value"),
-				bool_value: eb.ref("excluded.bool_value"),
-				json_value: eb.ref("excluded.json_value"),
-				page_link_id: eb.ref("excluded.page_link_id"),
-				media_id: eb.ref("excluded.media_id"),
-				group_id: eb.ref("excluded.group_id"),
-			})),
-		)
-		.returning(["fields_id"])
-		.execute();
+	const fieldsRes = await CollectionDocumentFieldsRepo.upsertMultiple({
+		items: fields.map((field) => {
+			return {
+				fieldsId: field.fields_id ?? undefined,
+				collectionDocumentId: data.document_id,
+				collectionBrickId: field.collection_brick_id,
+				key: field.key,
+				type: field.type,
+				groupId: field.group_id,
+				textValue: field.text_value,
+				intValue: field.int_value,
+				boolValue: field.bool_value,
+				jsonValue: field.json_value,
+				pageLinkId: field.page_link_id,
+				mediaId: field.media_id,
+				languageId: field.language_id,
+			};
+		}),
+	});
 
 	// delete fields not in fieldsRes
-	await serviceConfig.db
-		.deleteFrom("headless_collection_document_fields")
-		.where(
-			"collection_brick_id",
-			"in",
-			fields.map((field) => field.collection_brick_id),
-		)
-		.where(
-			"fields_id",
-			"not in",
-			fieldsRes.map((field) => field.fields_id),
-		)
-		.execute();
+	await CollectionDocumentFieldsRepo.deleteMultiple({
+		where: [
+			{
+				key: "collection_brick_id",
+				operator: "in",
+				value: fields.map((f) => f.collection_brick_id),
+			},
+			{
+				key: "fields_id",
+				operator: "not in",
+				value: fieldsRes.map((f) => f.fields_id),
+			},
+		],
+	});
 };
 
 export default upsertMultipleFields;
