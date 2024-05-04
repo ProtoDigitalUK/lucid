ef4d6cf01ee0e8bcb92ddabc8138a38658273f04
William Yallop <hello@williamyallop.com>
2024-05-04 21:43:10 +0100

    Update auth routes layout

--- apps/cms/src/layouts/AuthRoutes.tsx
+++ apps/cms/src/layouts/AuthRoutes.tsx
@@ -1,4 +1,5 @@
 import type { Component, JSXElement } from "solid-js";
+import LogoIcon from "@/assets/svgs/logo-icon.svg";
 
 interface AuthRoutesProps {
 	children?: JSXElement;
@@ -9,18 +10,15 @@ const AuthRoutes: Component<AuthRoutesProps> = (props) => {
 	// Render
 	return (
 		<div class="fixed top-0 left-0 bottom-0 right-0 flex ">
-			<div class="w-full xl:w-[40%] xl:min-w-[800px] h-full bg-container-3 overflow-y-auto flex items-center justify-center relative">
+			<div class="w-full h-full bg-container-3 overflow-y-auto flex items-center justify-center relative">
 				<div class="m-auto px-10 py-20 w-full max-w-[600px] ">
+					<img
+						src={LogoIcon}
+						alt="logo"
+						class="size-10 mx-auto mb-30"
+					/>
 					{props.children}
 				</div>
-			</div>
-			<div class="hidden w-[60%] bg-primary-base xl:flex items-center justify-center text-primary-contrast relative">
-				<span class="absolute inset-0 flex items-center">
-					<span class="block  border border-border opacity-80 w-full after:pb-[100%] after:block rotate-45 translate-x-1/2" />
-				</span>
-				<span class="absolute inset-0 flex items-center">
-					<span class="block  border border-border opacity-50 w-full after:pb-[100%] after:block rotate-45 translate-x-1/3" />
-				</span>
 			</div>
 		</div>
 	);
