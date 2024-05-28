d04c1c5948f41f6a820620e1450321074686e18b
William Yallop <hello@williamyallop.com>
2024-05-28 12:48:55 +0100

    Added basic documentation for bricks, collections and config

--- apps/documentation/astro.config.mjs
+++ apps/documentation/astro.config.mjs
@@ -4,96 +4,96 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://lucidcms.io",
-	integrations: [
-		starlight({
-			title: "Lucid CMS",
-			favicon: "./favicon.svg",
-			social: {
-				github: "https://github.com/ProtoDigitalUK/lucid",
-			},
-			logo: {
-				light: "./src/assets/dark-logo.svg",
-				dark: "./src/assets/dark-logo.svg",
-				replacesTitle: true,
-			},
-			components: {
-				Hero: "./src/components/Hero.astro",
-				SocialIcons: "./src/components/SocialIcons.astro",
-			},
-			customCss: ["./src/styles.css"],
-			sidebar: [
-				{
-					label: "Start Here",
-					items: [
-						{
-							label: "Getting Started",
-							link: "/getting-started/",
-						},
-						{
-							label: "For Non-Techies",
-							link: "/non-techies/",
-						},
-						{
-							label: "Why Lucid?",
-							link: "/why-lucid/",
-						},
-					],
-				},
-				{
-					label: "Configuration",
-					items: [
-						{
-							label: "Configuring Lucid",
-							link: "/guides/configuring-lucid/",
-						},
-						{
-							label: "Collections",
-							link: "/guides/collections/",
-						},
-						{
-							label: "Bricks",
-							link: "/guides/bricks/",
-						},
-					],
-				},
-				{
-					label: "Plugins",
-					items: [
-						// {
-						// 	label: "Forms",
-						// 	link: "/plugins/forms/",
-						// },
-						// {
-						// 	label: "Nested Documents",
-						// 	link: "/plugins/nested-documents/",
-						// },
-						// {
-						// 	label: "Cookie Consent",
-						// 	link: "/plugins/cookie-consent/",
-						// },
-						// {
-						// 	label: "Resend",
-						// 	link: "/plugins/resend/",
-						// },
-						{
-							label: "Nodemailer",
-							link: "/plugins/nodemailer/",
-						},
-						{
-							label: "S3",
-							link: "/plugins/s3/",
-						},
-						{
-							label: "Local Storage",
-							link: "/plugins/local-storage/",
-						},
-					],
-				},
-			],
-		}),
-		tailwind({
-			applyBaseStyles: false,
-		}),
-	],
+  site: "https://lucidcms.io",
+  integrations: [
+    starlight({
+      title: "Lucid CMS",
+      favicon: "./favicon.svg",
+      social: {
+        github: "https://github.com/ProtoDigitalUK/lucid",
+      },
+      logo: {
+        light: "./src/assets/dark-logo.svg",
+        dark: "./src/assets/dark-logo.svg",
+        replacesTitle: true,
+      },
+      components: {
+        Hero: "./src/components/Hero.astro",
+        SocialIcons: "./src/components/SocialIcons.astro",
+      },
+      customCss: ["./src/styles.css"],
+      sidebar: [
+        {
+          label: "Start Here",
+          items: [
+            {
+              label: "Getting Started",
+              link: "/getting-started/",
+            },
+            // {
+            // 	label: "For Non-Techies",
+            // 	link: "/non-techies/",
+            // },
+            // {
+            // 	label: "Why Lucid?",
+            // 	link: "/why-lucid/",
+            // },
+          ],
+        },
+        {
+          label: "Configuration",
+          items: [
+            {
+              label: "Configuring Lucid",
+              link: "/guides/configuring-lucid/",
+            },
+            {
+              label: "Collections",
+              link: "/guides/collections/",
+            },
+            {
+              label: "Bricks",
+              link: "/guides/bricks/",
+            },
+          ],
+        },
+        {
+          label: "Plugins",
+          items: [
+            // {
+            // 	label: "Forms",
+            // 	link: "/plugins/forms/",
+            // },
+            // {
+            // 	label: "Nested Documents",
+            // 	link: "/plugins/nested-documents/",
+            // },
+            // {
+            // 	label: "Cookie Consent",
+            // 	link: "/plugins/cookie-consent/",
+            // },
+            // {
+            // 	label: "Resend",
+            // 	link: "/plugins/resend/",
+            // },
+            {
+              label: "Nodemailer",
+              link: "/plugins/nodemailer/",
+            },
+            {
+              label: "S3",
+              link: "/plugins/s3/",
+            },
+            {
+              label: "Local Storage",
+              link: "/plugins/local-storage/",
+            },
+          ],
+        },
+      ],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
