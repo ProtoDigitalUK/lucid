daff6d9365408a82e45102d5a933ddc34c3e8055
William Yallop <hello@williamyallop.com>
2024-06-23 13:35:10 +0100

    Updated mock config to pass new key validation

--- packages/core/src/libs/config/mock-config/check-plugin-semver.ts
+++ packages/core/src/libs/config/mock-config/check-plugin-semver.ts
@@ -7,10 +7,14 @@ export default lucid.config({
 		database: async () => new Database("db.sqlite"),
 	}),
 	keys: {
-		encryptionKey: "encryptionKey",
-		cookieSecret: "cookieSecret",
-		refreshTokenSecret: "refreshTokenSecret",
-		accessTokenSecret: "accessTokenSecret",
+		encryptionKey:
+			"a9876b549a7d3d0350a5995a0c81a73452bccfa8b489f5dca8bd83ecbb6a8cba",
+		cookieSecret:
+			"a9876b549a7d3d0350a5995a0c81a73452bccfa8b489f5dca8bd83ecbb6a8cba",
+		refreshTokenSecret:
+			"a9876b549a7d3d0350a5995a0c81a73452bccfa8b489f5dca8bd83ecbb6a8cba",
+		accessTokenSecret:
+			"a9876b549a7d3d0350a5995a0c81a73452bccfa8b489f5dca8bd83ecbb6a8cba",
 	},
 	collections: [],
 	plugins: [
