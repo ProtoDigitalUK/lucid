7820274ee1fc5ef0d6b6fadad826739145d96ea4
William Yallop <hello@williamyallop.com>
2024-08-01 19:52:37 +0100

    Validation tests added for document custom fiedl

--- packages/core/src/libs/custom-fields/fields/document.test.ts
+++ packages/core/src/libs/custom-fields/fields/document.test.ts
@@ -1,6 +1,9 @@
+import T from "../../../translations/index.js";
 import { expect, test } from "vitest";
 import CustomFieldSchema from "../schema.js";
 import DocumentCustomField from "./document.js";
+import { validateField } from "../../../services/collection-document-bricks/checks/check-validate-bricks-fields.js";
+import CollectionBuilder from "../../../libs/builders/collection-builder/index.js";
 
 const CONSTANTS = {
 	collectionBrickId: "collection-pseudo-brick",
@@ -8,8 +11,159 @@ const CONSTANTS = {
 
 // -----------------------------------------------
 // Validation
+const DocumentCollection = new CollectionBuilder("collection", {
+	mode: "multiple",
+	title: "Test",
+	singular: "Test",
+	translations: true,
+})
+	.addDocument("standard_doc", {
+		collection: "page",
+	})
+	.addDocument("required_doc", {
+		collection: "page",
+		validation: {
+			required: true,
+		},
+	})
+	.addDocument("wrong_collection", {
+		collection: "wrong_collection",
+		validation: {
+			required: true,
+		},
+	});
 
-// TODO: needs adding
+test("successfully validate field - document", async () => {
+	// Standard
+	const standardValidate = validateField({
+		brickId: CONSTANTS.collectionBrickId,
+		field: {
+			key: "standard_doc",
+			type: "document",
+			value: 1,
+			localeCode: "en",
+		},
+		instance: DocumentCollection,
+		data: {
+			media: [],
+			users: [],
+			documents: [
+				{
+					id: 1,
+					collection_key: "page",
+				},
+			],
+		},
+	});
+	expect(standardValidate).toBe(null);
+
+	// Required
+	const requiredValidate = validateField({
+		brickId: CONSTANTS.collectionBrickId,
+		field: {
+			key: "required_doc",
+			type: "document",
+			value: 1,
+			localeCode: "en",
+		},
+		instance: DocumentCollection,
+		data: {
+			media: [],
+			users: [],
+			documents: [
+				{
+					id: 1,
+					collection_key: "page",
+				},
+			],
+		},
+	});
+	expect(requiredValidate).toBe(null);
+});
+
+test("fail to validate field - document", async () => {
+	// Required
+	const requiredValidate = {
+		exists: validateField({
+			brickId: CONSTANTS.collectionBrickId,
+			field: {
+				key: "required_doc",
+				type: "document",
+				value: 1,
+				localeCode: "en",
+			},
+			instance: DocumentCollection,
+			data: {
+				media: [],
+				users: [],
+				documents: [],
+			},
+		}),
+		null: validateField({
+			brickId: CONSTANTS.collectionBrickId,
+			field: {
+				key: "required_doc",
+				type: "document",
+				value: null,
+				localeCode: "en",
+			},
+			instance: DocumentCollection,
+			data: {
+				media: [],
+				users: [],
+				documents: [],
+			},
+		}),
+	};
+	expect(requiredValidate).toEqual({
+		exists: {
+			key: "required_doc",
+			brickId: CONSTANTS.collectionBrickId,
+			localeCode: "en",
+			groupId: undefined,
+			message: T("field_document_not_found"),
+		},
+		null: {
+			key: "required_doc",
+			brickId: CONSTANTS.collectionBrickId,
+			localeCode: "en",
+			groupId: undefined,
+			message: T("generic_field_required"),
+		},
+	});
+
+	// Wrong collection
+	const wrongCollectionValidate = validateField({
+		brickId: CONSTANTS.collectionBrickId,
+		field: {
+			key: "wrong_collection",
+			type: "document",
+			value: 1,
+			localeCode: "en",
+		},
+		instance: DocumentCollection,
+		data: {
+			media: [],
+			users: [],
+			documents: [
+				{
+					id: 1,
+					collection_key: "page",
+				},
+			],
+		},
+	});
+	expect(wrongCollectionValidate).toEqual({
+		key: "wrong_collection",
+		brickId: CONSTANTS.collectionBrickId,
+		localeCode: "en",
+		groupId: undefined,
+		message: T("field_document_collection_key_mismatch", {
+			received: "page",
+			expected: "wrong_collection",
+		}),
+	});
+});
 
 // -----------------------------------------------
 // Custom field config
