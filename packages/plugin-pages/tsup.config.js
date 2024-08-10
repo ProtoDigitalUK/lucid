3bde7ea5f6c836c6c60b31e948abefe29157b416
William Yallop <hello@williamyallop.com>
2024-08-10 12:18:20 +0100

    Added watch to playground for plugin-pages package and restart.txt gen on build finish to plugin-pages

--- packages/plugin-pages/tsup.config.js
+++ packages/plugin-pages/tsup.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from "tsup";
+import fs from "fs-extra";
+import { join } from "node:path";
 
 export default defineConfig({
-	entry: ["src/index.ts"],
-	dts: true,
-	format: "esm",
-	shims: false,
-	sourcemap: true,
-	clean: true,
-	metafile: true,
+  entry: ["src/index.ts"],
+  dts: true,
+  format: "esm",
+  shims: false,
+  sourcemap: true,
+  clean: true,
+  metafile: true,
+  async onSuccess() {
+    setTimeout(() => {
+      fs.ensureFileSync(join(process.cwd(), "restart.txt"));
+      fs.writeFileSync(
+        join(process.cwd(), "restart.txt"),
+        new Date().toISOString()
+      );
+    }, 300);
+  },
 });
