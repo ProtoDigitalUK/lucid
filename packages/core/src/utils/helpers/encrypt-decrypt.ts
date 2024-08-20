2020f3edb22a0932cff5d89b9279f1004d8aa181
William Yallop <hello@williamyallop.com>
2024-08-20 22:52:17 +0100

    Type error for createCipheriv fixed

--- packages/core/src/utils/helpers/encrypt-decrypt.ts
+++ packages/core/src/utils/helpers/encrypt-decrypt.ts
@@ -1,31 +1,32 @@
+import T from "../../translations/index.js";
 import crypto from "node:crypto";
+import LucidError from "../errors/lucid-error.js";
 
 const encryptionKeyToHex = (encryptionKey: string) =>
 	crypto
 		.createHash("sha512")
 		.update(encryptionKey)
 		.digest("hex")
-		.substring(0, 64);
+		.substring(0, 32);
 
 export const encrypt = (secret: string, encryptionKey: string) => {
-	const iv = crypto.randomBytes(16);
-	const cipher = crypto.createCipheriv(
-		"aes-256-cbc",
-		Buffer.from(encryptionKeyToHex(encryptionKey), "hex"),
-		iv,
-	);
+	const key = encryptionKeyToHex(encryptionKey);
+	const iv = crypto.randomBytes(16).toString("hex").slice(0, 16);
+	const cipher = crypto.createCipheriv("aes-256-cbc", key, iv);
 	let encrypted = cipher.update(secret, "utf8", "hex");
 	encrypted += cipher.final("hex");
-	return `${iv.toString("hex")}:${encrypted}`;
+	return `${iv}:${encrypted}`;
 };
 
 export const decrypt = (encryptedSecret: string, encryptionKey: string) => {
 	const [iv, encrypted] = encryptedSecret.split(":");
-	const decipher = crypto.createDecipheriv(
-		"aes-256-cbc",
-		Buffer.from(encryptionKeyToHex(encryptionKey), "hex"),
-		Buffer.from(iv as string, "hex"),
-	);
+	if (!iv || !encrypted) {
+		throw new LucidError({
+			message: T("invalid_encrypted_secret"),
+		});
+	}
+	const key = encryptionKeyToHex(encryptionKey);
+	const decipher = crypto.createDecipheriv("aes-256-cbc", key, iv);
 	let decrypted = decipher.update(encrypted as string, "hex", "utf8");
 	decrypted += decipher.final("utf8");
 	return decrypted.toString();
