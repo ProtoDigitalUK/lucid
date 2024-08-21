5bb177956cefa07854db48666453669922149207
William Yallop <hello@williamyallop.com>
2024-08-21 18:11:34 +0100

    Removed Nodemon dependency from playground and removed post build script on core and plugin-pages that was used to restart playground

--- packages/core/tsup.config.js
+++ packages/core/tsup.config.js
@@ -1,5 +1,3 @@
-import fs from "fs-extra";
-import { join } from "node:path";
 import { defineConfig } from "tsup";
 
 export default defineConfig({
@@ -17,13 +15,4 @@ export default defineConfig({
   sourcemap: true,
   clean: true,
   metafile: true,
-  async onSuccess() {
-    setTimeout(() => {
-      fs.ensureFileSync(join(process.cwd(), "restart.txt"));
-      fs.writeFileSync(
-        join(process.cwd(), "restart.txt"),
-        new Date().toISOString()
-      );
-    }, 300);
-  },
 });
--- packages/plugin-pages/tsup.config.js
+++ packages/plugin-pages/tsup.config.js
@@ -1,6 +1,4 @@
 import { defineConfig } from "tsup";
-import fs from "fs-extra";
-import { join } from "node:path";
 
 export default defineConfig({
   entry: ["src/index.ts"],
@@ -10,13 +8,4 @@ export default defineConfig({
   sourcemap: true,
   clean: true,
   metafile: true,
-  async onSuccess() {
-    setTimeout(() => {
-      fs.ensureFileSync(join(process.cwd(), "restart.txt"));
-      fs.writeFileSync(
-        join(process.cwd(), "restart.txt"),
-        new Date().toISOString()
-      );
-    }, 300);
-  },
 });
